2f8cb8047009b7627004d52b55093ae7ca54f36c
mrnone <mrnone@localhost.localdomain>
2021-05-09 00:23:50 -0500

    Add deployment

---
+++ test/Readme.txt
@@ -0,0 +1,11 @@
+Deploying project to rinkeby network:
+
+truffle migrates --network rinkeby
+
+Reset the migrations in case of faliure:
+
+truffle migrate --reset --network rinkeby
+
+Check the smart contracs on etherscan:
+
+truffle networks
---
+++ truffle-config.js
@@ -0,0 +1,39 @@
+//este archivo es el que permite la comunicación con el blockchain
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() { // give the connection to this specific network
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 4
+      //networkCheckTimeout: 10000
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg"
+    }
+  }
+}
